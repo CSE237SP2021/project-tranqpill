b7ae1dbb2361b04471a3d593aee3de2d5156cce9
zachisaacson <zachary.isaacson@wustl.edu>
2021-04-10 16:59:03 -0500

    added juinit test

--- src/tranqpill/CheckersTests.java
+++ src/tranqpill/CheckersTests.java
@@ -5,6 +5,25 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class CheckersTests {
+	
+	@Test
+	void testPieceProperties() {
+		WhiteSingle ws = new WhiteSingle();
+		assertEquals(ws.isBlack(),false);
+		assertEquals(ws.isDouble(),false);
+		
+		WhiteDouble wd = new WhiteDouble();
+		assertEquals(wd.isBlack(),false);
+		assertEquals(wd.isDouble(),true);
+		
+		BlackSingle bs = new BlackSingle();
+		assertEquals(bs.isBlack(),true);
+		assertEquals(bs.isDouble(),false);
+		
+		BlackDouble bd = new BlackDouble();
+		assertEquals(bd.isBlack(),true);
+		assertEquals(bd.isDouble(),true);
+	}
 
 	@Test
 	void testEmptyBoard() {
