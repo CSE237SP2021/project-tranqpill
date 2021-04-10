f6bf8d4863db16ef003c50dcafa0e7570e83a073
zachisaacson <zachary.isaacson@wustl.edu>
2021-04-10 16:53:42 -0500

    create difference piece classes with inheritence

--- src/tranqpill/Piece.java
+++ src/tranqpill/Piece.java
@@ -1,34 +1,7 @@
 package tranqpill;
 
-public class Piece {
+public interface Piece {
 	// w and b represent white and black queens, W and B represent white and black kings
-	private char type; 
-	private boolean isBlack;
-	
-	public Piece(char s) {
-		type = s;
-		if (s == 'w' || s == 'W') {
-			this.isBlack = false;
-		} else {
-			this.isBlack = true;
-		}
-	}
-	
-	public Piece(Piece p) {
-		this.type = p.getType();
-		this.isBlack = p.isBlack();
-	}
-	
-	public char getType() {
-		return this.type;
-	}
-	
-	public boolean isBlack() {
-		return this.isBlack;
-	}
-	
-	@Override
-	public String toString() {
-		return "" + this.type;
-	}
+	public boolean isBlack();
+	public boolean isDouble();
 }
---
+++ src/tranqpill/WhiteSingle.java
@@ -0,0 +1,18 @@
+package tranqpill;
+
+public class WhiteSingle implements Piece{
+
+	public boolean isBlack() {
+		return false;
+	}
+	
+	public boolean isDouble() {
+		return false;
+	}
+	
+	@Override
+	public String toString() {
+		return "w";
+	}
+
+}
